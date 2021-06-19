c0de31568652361130bdf6f897929539b2097435
Camilo <camilogm3@gmail.com>
2021-06-19 16:15:28 -0300

    corrección index api

--- pages/api/team/index.js
+++ pages/api/team/index.js
@@ -6,9 +6,6 @@ const allTeam = async (request = IncomingMessage, response = ServerResponse) => 
     const allEntries = await db.getAll()
     const length = allEntries.length
     response.status(200).json({length, data: allEntries})
-}
-
-const nueva = async (request = IncomingMessage, response = ServerResponse) => {
     if(request.config.method === 'put'){
         const db = new Database()
         await db.postById(request.data)
@@ -17,4 +14,3 @@ const nueva = async (request = IncomingMessage, response = ServerResponse) => {
 }
 
 export default allTeam
-export default nueva
