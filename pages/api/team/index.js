284653c6ec7bc7ea4698ae74ef2497aa6e06682f
Camilo <camilogm3@gmail.com>
2021-06-19 13:27:05 -0300

    acceso público  api

--- pages/api/team/index.js
+++ pages/api/team/index.js
@@ -1,7 +1,9 @@
 import { IncomingMessage, ServerResponse } from 'http'
 import Database from '@database/db'
+import enablePublicAccess from 'cors'
 
 const allTeam = async (request = IncomingMessage, response = ServerResponse) => {
+    enablePublicAccess(request, response)
     const db = new Database()
     const allEntries = await db.getAll()
     const length = allEntries.length
