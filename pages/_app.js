cb881571e4d98fffb91cb1c931ebd3751bf5d31a
Camilo <camilogm3@gmail.com>
2021-06-25 23:32:07 -0300

    Información meta

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import App from 'next/app';
+import Head from 'next/head'
 import {UserContextProvider} from '@context/UserContext'
 import { TeamContextProvider } from '@context/TeamContext';
 import Layout from '@components/Layout';
@@ -10,6 +11,11 @@ class MyApp extends App {
     return (
       <UserContextProvider>
         <TeamContextProvider>
+          <Head>
+            <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+            <title>SuperTeam</title>
+            <meta name="description" content="Accede a cientos de superheroes y villanos de diferentes universos y crea tu equipo."></meta>
+          </Head>
           <Layout>
             <Component {...pageProps} />
           </Layout>
